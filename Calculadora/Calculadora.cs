a337c32d3eb5af9413d213f75195039d7d3aa7af
Wesley Souza <Wesley-souza8@hotmail.com>
2025-09-02 17:31:21 -0300

    Implementação da divisão, ajustes dos métodos para decimal e colocar um tratamento caso tenha divisão por zero

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -8,24 +8,48 @@ namespace Calculadora
         {
             switch(operacao.operador)
             {
-                case '+': operacao.resultado= soma(operacao);break;
-                case '-': operacao.resultado = subtracao(operacao);break;
-                case '*': operacao.resultado = multiplicacao(operacao);break;
-                default: operacao.resultado = 0; break;
+                case '+':
+                    operacao.resultado= soma(operacao);
+                break;
+                case '-':
+                    operacao.resultado = subtracao(operacao);
+                break;
+                case '*': 
+                    operacao.resultado = multiplicacao(operacao);
+                break;
+
+                // Implementação da divisão
+                case '/': 
+                    operacao.resultado = divisao(operacao);
+                break;
+                default:
+                    operacao.resultado = 0;
+                break;
             }
             return operacao;
         }
-        public int soma(Operacoes operacao)
+
+        // Ajuste dos métodos para usar decimal
+        public decimal soma(Operacoes operacao)
         {
             return operacao.valorA + operacao.valorB;
         }
-        public int subtracao(Operacoes operacao)
+        public decimal subtracao(Operacoes operacao)
         {
             return operacao.valorA - operacao.valorB;
         }
-        public int multiplicacao(Operacoes operacao)
+        public decimal multiplicacao(Operacoes operacao)
         {
             return operacao.valorA * operacao.valorB;
+        }       
+        public decimal divisao(Operacoes operacao)
+        {
+            // Tratamento para divisão por zero
+            if (operacao.valorB == 0)
+            {
+                throw new DivideByZeroException("Divisão por zero não é permitida.");
+            }
+            return operacao.valorA / operacao.valorB;
         }
        
     }
